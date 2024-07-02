520ccb3f5510e1877cd2e57d5a15100c286a43ea
Zolboonoo <90175517+Zolboonoo@users.noreply.github.com>
2024-07-02 14:42:13 +0900

    user delete function

--- SampleWeb/src/main/java/com/example/demo/constant/MessageConst.java
+++ SampleWeb/src/main/java/com/example/demo/constant/MessageConst.java
@@ -10,7 +10,8 @@ public class MessageConst {
 	public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
 	public static final String SIGNUP_EXISTED_LOGIN_ID = "signup.existedLoginId";
 	public static final String SIGNUP_RESIST_SUCCEED = "signup.resistSucceed";
-	
+	public static final String USERLIST_NON_EXISTED_LOGIN_ID = "userList.nonExistedLoginId";
+	public static final String USERLIST_DELETE_SUCCEED = "userList.deleteSucceed";	
 	
 }
 	
